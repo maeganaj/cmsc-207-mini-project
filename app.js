22d72bd65b9190898ece241d3e6fa2a410b85ad4
aj <maeganajjocson@gmail.com>
2025-03-17 05:06:23 +0800

    burger clickable

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ burger.addEventListener('click',function(){
 close.addEventListener('click',function(){
     mobileNav.classList.remove('open');
 });
+
+let click1 = document.querySelector('.click1');
+let click2 = document.querySelector('.click2');
+let click3 = document.querySelector('.click3');
+
+click1.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+});
+
+click2.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+});
+
+click3.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+});
